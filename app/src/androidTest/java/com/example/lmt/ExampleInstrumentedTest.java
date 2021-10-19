6e2ebf72aa1503e6efc730837c575936642f1e3b
DroidFreak32 <rushabshah32@gmail.com>
2021-10-20 00:27:47 +0530

    tmp

--- app/src/androidTest/java/com/example/lmt/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/lmt/ExampleInstrumentedTest.java
@@ -20,6 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
         assertEquals("com.example.lmt", appContext.getPackageName());
     }
 }
